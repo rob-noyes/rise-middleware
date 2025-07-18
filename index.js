0b3c95dd42b40ada48879b1cfea5bb0c45c37e38
rob-noyes <robby.noyes@gmail.com>
2025-07-18 09:22:49 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/rise/initiate', (req, res) => {
+    const token = req.query.token;
+    if (!token) {
+        return res.status(400).send('Missing token');
+    }
+    const appId = 'YOUR_APP_ID';
+    const redirectUrl = encodeURIComponent('https://YOUR-RENDER-URL.onrender.com/rise/callback');
+    const installUrl = `https://platform.rise.ai/installer/install?token=${token}&appId=${appId}&redirectUrl=${redirectUrl}`;
+
+    res.redirect(installUrl);
+});
+
+app.get('/rise/callback', (req, res) => {
+    const { code, instance_id } = req.query;
+    if (!code || !instance_id) {
+        return res.status(400).send('Missing code or instance_id');
+    }
+
+    console.log('Received:', { code, instance_id });
+
+    res.send(`Success!<br>Code: ${code}<br>Instance ID: ${instance_id}`);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
